5dd337f75f6ff21811eafcdea1a8e0afee636b84
Adolfo Gante <adolfogante@gmail.com>
2025-03-17 02:03:51 -0700

    creates uint8(1,224,224,3) datasets for quantized models

---
+++ preprocess_images_to_datasets_quantized.py
@@ -0,0 +1,35 @@
+import numpy as np
+from PIL import Image
+import qai_hub as hub
+from os import listdir
+
+#
+# UPDATE THESE FUNCTIONS BASED ON WHAT YOUR AI MODEL WANTS
+# EX: wideresnet50 wants a 'image_tensor: float32[1, 224, 224, 3]'
+#
+
+# Function to preprocess the image
+def preprocess_image(image_path):
+	image = Image.open(image_path).convert("RGB")
+	image = image.resize((224, 224))  # Resize to 224x224
+	image = np.array(image).astype(np.uint8)
+	image = np.expand_dims(image, axis=0)  # Add batch dimension
+	return image
+
+# Function to create an h5 dataset given a list of image_tensors
+def construct_h5_dataset(image_paths, set_num):
+	processed_images = []
+
+	for image in image_paths:
+		print(f"Preprocessing: {image}")
+		image = preprocess_image(image)	
+		processed_images.append(image)
+
+	print("Initializing Upload! 🔥🔥🔥")
+	data = dict(image_tensor = processed_images)
+	dataset = hub.upload_dataset(data)
+	dataset.download(f"./preprocess_image_datasets_quantized/dataset_{set_num}")
+
+for i in range(50):
+	image_paths = [f"./split_imagenet_folders/imagenet_set_{i + 1}/{img}" for img in listdir(f"./split_imagenet_folders/imagenet_set_{i + 1}")]
+	construct_h5_dataset(image_paths, i + 1)
