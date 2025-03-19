1859d8d730de2a94d55afb64c9ef4fc13dc06d60
Adolfo Gante <adolfogante@gmail.com>
2025-03-19 11:52:27 -0700

    added comments

--- benchmark.py
+++ benchmark.py
@@ -11,9 +11,13 @@ model_name = "wideresnet50_quantized"
 library_name = "tflite"
 results_dir = "benchmarked_model_datasets/wideresnet50_quantized_tflite_s24"
 
+### Construct image datasets
+
 # for i in range(50):
 # 	image_paths = [f"./imagenet_50k/imagenet_set_{i + 1}/{img}" for img in listdir(f"./imagenet_50k/imagenet_set_{i + 1}")]
 # 	construct_datasets(image_paths, image_dataset_dir, i + 1, InputSpec.QUANTIZED)
+
+### Inference Image datasets
 
 image_dataset_dir = "image_datasets_quantized"
 dataset_paths = [
@@ -43,9 +47,13 @@ thread2.start()
 thread1.join()
 thread2.join()
 
+### Process results from inference
+
 result_paths = [f"./{results_dir}/" + result for result in listdir(f"./{results_dir}")]
 result_paths.sort(key=extract_number)
 process_results(result_paths, "mappings/class_index.json", "mappings/synset.json")
+
+### Calculate accuracy based on processed results
 
 results_json = "results.json"
 ground_truth_json = "./mappings/val_ground_truth.json"
